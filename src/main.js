e8fbde0a8eadcda5314bb1cec808dc143cf554c4
Landon <clandon491@gmail.com>
2026-08-20 20:08:12 +0200

    feat: initialize machine time estimator with app

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './styles.css';
 import './postnet-ui.css';
 import './postnet-board.css';
 import { installWizardValidation } from './app/wizardValidation.js';
+import { installMachineTimeEstimator } from './utils/machineTimeEstimator.js';
 import { initApp } from './app/app.js';
 
 installWizardValidation();
+installMachineTimeEstimator();
 initApp();
