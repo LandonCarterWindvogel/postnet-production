5c5acc9077062130c3d4824df2f61e3a6807a1f2
Landon <clandon491@gmail.com>
2026-08-24 16:26:03 +0200

    feat: install live machine time estimator UI

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import './styles.css';
 import './postnet-ui.css';
 import './postnet-board.css';
 import { installWizardValidation } from './app/wizardValidation.js';
+import { installMachineEstimatorUi } from './app/machineEstimatorUi.js';
 import { initApp } from './app/app.js';
 
 installWizardValidation();
+installMachineEstimatorUi();
 initApp();
